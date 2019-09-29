926702996bba0eb1a85c609d403969f404dd42ca
Amgad Malik <amgadmalik@yahoo.com>
2019-09-29 14:40:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :name
+  
+  def intialize(name)
+    @name = name
+    brinding.pry
+    @roster ={}
+  end 
+  
+  def add_student(student_name, grade)
+   
+    @roster[grade] = [] unless @roster.key?(grade)
+    @roster[grade] << student_name
+    
+  end 
+  
+  #school = School.new
+end 
